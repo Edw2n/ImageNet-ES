f0602d5a88fb139a70083da9346e49f62fc7b4e7
Edw2n <esbaek@hcil.snu.ac.kr>
2024-03-29 13:03:39 +0900

    general user configs

---
+++ configs/user_configs.py
@@ -0,0 +1,9 @@
+# Need to update
+
+IMAGENET_ES_ROOT_DIR = "/path/to/ImageNet-ES-ROOT-DIR/"
+SWIN_PT = "/path/to/Swin-weight-file"
+RESNET18_PT = "/path/to/Resnet18-weight-file"
+EN_PT = "/path/to/EfficientNet-weight-file"
+VIT_PT = "/path/to/vit-weight-file"
+
+TARGET_OOD_POSTPROCESSORS = ['msp', 'odin', 'react', 'vim', 'ash']
